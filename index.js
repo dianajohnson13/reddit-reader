c2997af5d18f775a46ebf825c6eee6a6f691732e
Diana <dianajohnson13@gmail.com>
2016-08-24 22:25:05 -0700

    request hot / front page posts from reddit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var reddit = require('redwrap');
 
 var app = express();
 
@@ -8,3 +9,17 @@ app.listen(port);
 console.log("Listening on port: " + port);
 
 app.use(express.static(__dirname + '/client'));
+
+app.get('/api/hot', function(req, res) {
+  getHot(req, res)
+});
+
+function getHot(req, resp) {
+  reddit.list('hot', function(err, data, r){
+    if (err) {
+      resp.status(500).send("Error getting reddit hot posts!");
+    } else {
+      resp.status(200).send(data["data"]["children"]);
+    }
+  });
+}

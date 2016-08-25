840c99f8457205654a4641abf455fbf799e2e729
Diana <dianajohnson13@gmail.com>
2016-08-25 08:27:01 -0700

    condense use of multiple controllers and views

--- client/app/app.js
+++ client/app/app.js
@@ -1,18 +1,13 @@
 angular.module('reader', [
   'reader.services',
-  'reader.hot',
   'reader.subreddit',
   'ngRoute'
 ])
 .config(function($routeProvider, $httpProvider) {
   $routeProvider
-    .when('/hot', {
-      templateUrl: 'app/hot/hot.html',
-      controller: 'HotController'
-    })
-    .when('/', {
-      templateUrl: 'app/hot/hot.html',
-      controller: 'HotController'
+    .when('/*', {
+      templateUrl: 'app/subreddit/subreddit.html',
+      controller: 'SubRedditController'
     })
     .otherwise({
       templateUrl: 'app/subreddit/subreddit.html',
--- client/app/subreddit/subreddit.js
+++ client/app/subreddit/subreddit.js
@@ -2,16 +2,39 @@ angular.module('reader.subreddit', [])
 
 .controller('SubRedditController', function ($scope, PostGetter, $location) {
   $scope.data = {};
-  $scope.subreddits = []; 
+  $scope.subreddits = [];
 
-  $scope.getSubreddits = function() {
-    urlpath = $location.path();
+  $scope.getWhatsHot = function() {
+    console.log('getWhatsHot')
+    PostGetter.getWhatsHot().then(function(resp) {
+      $scope.data.posts = resp.data
+      console.log($scope.data.posts)
+    });
+  };
+
+  $scope.getSubreddits = function(urlpath) {
+    console.log('getSubreddits')
     $scope.subreddits = urlpath.slice(1, urlpath.length).split("+");
 
     PostGetter.getSubreddits($scope.subreddits).then(function(resp) {
       $scope.data.posts = resp.data;
-      console.log(resp.data)
+      console.log($scope.data.posts)
     });
+  };
+
+  $scope.directUrlPath = function() {
+    urlpath = $location.path();
+    console.log(urlpath)
+    switch (urlpath) {
+      case "/":
+        $scope.getWhatsHot();
+        break;
+      case "/hot":
+        $scope.getWhatsHot();
+        break;
+      default:
+        $scope.getSubreddits(urlpath);
+    }
   }();
 
 });
